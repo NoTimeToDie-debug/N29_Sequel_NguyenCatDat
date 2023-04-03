4d8e72b7c6074642ec580a4e6959e72459ef5626
NoTimeToDie-debug <82013966+NoTimeToDie-debug@users.noreply.github.com>
2023-04-03 22:17:52 +0700

    giai doan 2

--- controllers/foodController.js
+++ controllers/foodController.js
@@ -2,6 +2,8 @@ const {Op, where} = require('sequelize');
 const initModels = require('../models/init-models');
 const sequelize = require('../models/index');
 const food = require('../models/food');
+const user = require('../models/user');
+const like_res = require('../models/like_res');
 const model = initModels(sequelize);
 
 
@@ -48,19 +50,96 @@ const updateFood = async (req , res) => {
             desc,
             type_id
         }
-        let data = await model.food.update(modelUpdate, {
+        await model.food.update(modelUpdate, {
             where: {
                 food_id
             }
         });
-        res.status(200).send(data);
+        let data = await model.food.findAll();
+        res.status(200).send(JSON.stringify(data,null,4));
     } catch (error) {
         res.status(500).send('lỗi BE')
     }
 }
 
+//lấy danh sách like theo nhà hàng và user
+const getLikeResUser = async (req, res) => {
+    try {
+        let data = await model.like_res.findAll({
+            include: ["re","user"]
+
+        });
+        res.status(200).send(data);
+    } catch (error) {
+        res.status(500).send("lỗi BE");
+    }
+    
+}
+
+//like nhà hàng
+const createLikeRes = async (req, res) => {
+    try {
+        const {user_id , res_id , date_like } = req.body;
+        let newLike = {
+            user_id,	
+            res_id,	
+            date_like
+        }
+
+        let data = await model.like_res.create(newLike);
+        res.status(200).send(data);
+        
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+//unlike nhà hàng
+const removeLikeRes = async (req, res) => {
+    try {
+        let {user_id,res_id} = req.params;
+        let existData = model.like_res.findAll({
+            where:{
+                user_id,
+                res_id
+            }
+        })
+        if (existData) {
+            let data = await model.like_res.destroy({
+                where:{
+                    user_id,
+                    res_id
+                }
+            });
+            res.status(200).send(data);
+        } else {
+            res.send("dữ liệu ko tồn tại")
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+const countLikeRes = async (req, res) => {
+    
+        let data = await model.restaurant.findAndCountAll({
+            // where: {
+            //     res_name: {
+            //         [Op.like]: '%kfc%'
+            //     }
+            // },
+            include: ["like_res"]
+        })
+        res.status(200).send(data);
+}
+
 module.exports = {
     getFood,
     createFood,
-    updateFood
+    updateFood,
+    getLikeResUser,
+    countLikeRes,
+    createLikeRes,
+    removeLikeRes
 }
--- routes/foodRoute.js
+++ routes/foodRoute.js
@@ -1,11 +1,15 @@
 const express = require('express');
-const { getFood, createFood, updateFood } = require('../controllers/foodController');
+const { getFood, createFood, updateFood, getLikeResUser, countLikeRes, createLikeRes, removeLikeRes } = require('../controllers/foodController');
 const foodRouter = express.Router();
 
 foodRouter.get("/get-food",getFood);
+foodRouter.get("/get-like-res-user",getLikeResUser);
+foodRouter.get("/count-like-res",countLikeRes);
 
 foodRouter.post("/create-food",createFood);
+foodRouter.post("/create-like-res",createLikeRes);
 
 foodRouter.put("/update-food/:food_id",updateFood);
+foodRouter.put("/remove-like-res/:user_id/:res_id",removeLikeRes);
 
 module.exports = foodRouter;
